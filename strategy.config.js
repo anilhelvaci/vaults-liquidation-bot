0d813d94dca51f7ffe5a10fad3ae5c553b260a39
anilhelvaci <anilhelvaci96@gmail.com>
2023-10-30 10:34:53 +0300

    #6 #7 config update

--- strategy.config.js
+++ strategy.config.js
@@ -1,7 +1,59 @@
+/**
+ * Built-in configurations:
+ * - delta is exact, spending is flash
+ * - delta is exact, spending is controlled
+ * - delta is percentage, spending is flash
+ * - delta is percentage, spending is controlled
+ */
+
 export default [
     {
-        // Strict difference
-        delta: 1n,
+        delta: {
+            type: 'exact',
+            value: 500000n // 0.5 IST
+        },
+        spend: {
+            type: 'flash'
+        },
+        credit: 100_000_000n, // 100 IST credit
+        collateralName: 'ATOM',
+        bookId: 0,
+    },
+    {
+        delta: {
+            type: 'exact',
+            value: 500000n // 0.5 IST
+        },
+        spend: {
+            type: 'controlled',
+            controlFactor: 10n, // amountIn = credit / controlFactor
+        },
+        credit: 100_000_000n, // 100 IST credit
+        collateralName: 'ATOM',
+        bookId: 0,
+    },
+    {
+        delta: {
+            type: 'percentage',
+            value: 5n // 5% off from the external price
+        },
+        spend: {
+            type: 'flash',
+        },
+        credit: 100_000_000n, // 100 IST credit
+        collateralName: 'ATOM',
+        bookId: 0,
+    },
+    {
+        delta: {
+            type: 'percentage',
+            value: 5n // 5% off from the external price
+        },
+        spend: {
+            type: 'controlled',
+            controlFactor: 10n, // amountIn = credit / controlFactor
+        },
+        credit: 100_000_000n, // 100 IST credit
         collateralName: 'ATOM',
         bookId: 0,
     },
